c15a6e2c7259b2322f00faa3805c9e3f921e59f4
Mark Spiker <kb8uvv@gmail.com>
2018-03-13 20:26:49 -0400

    Create and Read support

--- MongoDB-CRUD/server.js
+++ MongoDB-CRUD/server.js
@@ -1,15 +1,16 @@
 const express = require('express');     // npm install express --save
 const bodyParser = require('body-parser')   // npm install body-parser --save
-const app = express();
 const mongoClient = require('mongodb').MongoClient      // npm install mongodb --save
+const app = express();
 
 var db
 
 app.use(bodyParser.urlencoded({extended: true}))    // important to do this before starting the listner
+app.set('view engine', 'ejs')       // set the view engine
 
 // Questioning the order in which we establish a connection to the MongoDB, maybe this should be at the point 
 // the handler picks up a request in which we need the database.  
-mongoClient.connect('', (err, client) => {
+mongoClient.connect('mongodb://nodejs:nodejs3000!@ds257858.mlab.com:57858/spik01', (err, client) => {
     if (err) return console.log(err)
     db = client.db('spik01')
     app.listen(3000, function() {
@@ -17,9 +18,11 @@ mongoClient.connect('', (err, client) => {
     })
 })
 
-// All your handlers here...
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    db.collection('quotes').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        res.render('index.ejs', {quotes: result})
+    })
 })
 
 app.post('/quotes', (req, res) => {
